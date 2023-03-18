a26a956a928208cef5f46892fed840af234c4039
johannes <johannes.lb.holst@gmail.com>
2023-03-18 12:32:28 +0100

    starry night!

--- sketch.js
+++ sketch.js
@@ -1,8 +1,38 @@
+var canvas;
+let time = 0.;
+let stars = [];
+let numstar = 400;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
+	canvas = createCanvas(windowWidth, windowHeight);
+	canvas.position(0, 0);
+	canvas.style('z-index', '-1');
+	for (let i=0; i<numstar; i++) {
+		stars[i] = []
+		stars[i][0] = width * random()
+		stars[i][1] = height * random()
+		stars[i][2] = random()
+	}
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-	// put drawing code here
+	noStroke();
+	
+	let h = 10
+	const n = height / h
+	for (let i=0; i<n; i++) {
+		fill(i, 0, 51);
+		rect(0, h*i, width, h);
+	}
+
+	for (let i=0; i<numstar; i++) {
+		fill(255);
+		ellipse(stars[i][0], stars[i][1], 1.2 * noise(time + 10*i) + stars[i][2]);
+	}
+
+	time = time + 0.1
 }
